e948d5a42b8c8ea3984cbd8c12da85e831a61082
Gan Shao Hong <shaohong.g97@gmail.com>
2025-03-01 14:47:07 +0800

    add more leetcode qns

--- README.md
+++ README.md
@@ -11,6 +11,14 @@ LeetCode No. | Question | Solution | Tags | Time Complexity | Space Complexity
 --- | :--- | :--- | :---:  | :---: | :---:
 88 | [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150) | [merge_sort](./solutions/leetCodeTopInterview.py) | `Two pointers`, `Sorting` | O(N+M) | O(1)
 27 | [Remove Element](https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150) | [removeElement](./solutions/leetCodeTopInterview.py) | `Two pointers`, `Sorting` | O(N) | O(1)
+26 | [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150) | [removeDuplicates](./solutions/leetCodeTopInterview.py) | `Two pointers`, `Array` | O(N) | O(1)
+80 | [Remove Duplicates from Sorted Array II](https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150) | [removeDuplicates2](./solutions/leetCodeTopInterview.py) | `Two pointers`, `Array` | O(N) | O(1)
+169 | [Majority Element](https://leetcode.com/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150) | [majorityElement](./solutions/leetCodeTopInterview.py) | `Boyer–Moore majority vote algorithm`, `Sort` | O(N) | O(1)
+189 | [Rotate Array](https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150) | [rotate](./solutions/leetCodeTopInterview.py) | `Array` | O(N) | O(1)
+121 | [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150) | [maxProfit](./solutions/leetCodeTopInterview.py) | `Array` | O(N) | O(1)
+122 | [Best Time to Buy and Sell Stock II](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/?envType=study-plan-v2&envId=top-interview-150) | [maxProfit2](./solutions/leetCodeTopInterview.py) | `Array` | O(N) | O(1)
+55 | [Jump Game](https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150) | [canJump](./solutions/leetCodeTopInterview.py) | `Array` | O(N) | O(1)
+45 | [Jump Game II](https://leetcode.com/problems/jump-game-ii/?envType=study-plan-v2&envId=top-interview-150) | [jump](./solutions/leetCodeTopInterview.py) | `Array`, `Dynamic Programming` | O(MN) | O(N)
 
 ## Resources
 - [Leetcode patterns scan](https://seanprashad.com/leetcode-patterns/)
--- solutions/leetCodeTopInterview.py
+++ solutions/leetCodeTopInterview.py
@@ -44,3 +44,156 @@ def removeElement(nums, val):
                 first_pointer += 1
 
         return first_pointer
+
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    
+    if (len(nums) < 2):
+        return len(nums)
+
+    current_idx = 0
+
+    for i in range(1, len(nums)):
+        # swap if moving > curr
+        if nums[i] > nums[current_idx]:
+            current_idx += 1
+            nums[current_idx] = nums[i]
+
+    return current_idx +1
+
+def removeDuplicates2(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if (len(nums) <= 2):
+        return len(nums)
+
+    current_idx = 1
+    for i in range(2, len(nums)):
+        if nums[i] != nums[current_idx -1]:
+            current_idx += 1
+            nums[current_idx] = nums[i]
+    return current_idx +1
+
+def majorityElement(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    # Sort solution
+    # return sorted(nums)[len(nums)//2]
+    
+    # Boyer–Moore majority vote algorithm
+    candidate = None
+    vote = 0
+
+    for each_num in nums:
+        if vote == 0:
+            candidate = each_num
+        vote += (1 if each_num == candidate else -1)
+    return candidate
+
+def rotate(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    k = k % n
+    nums[:] = nums[n-k:] + nums[:n-k] # O(n) time - O(n) space (concat)
+
+    # Alternative O(n) time -  O(1) space
+    nums.reverse()
+    nums[:k] = reversed(nums[:k])
+    nums[k:] = reversed(nums[k:])
+
+def maxProfit(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    max_profit = 0
+    start = prices[0]
+
+    for price in prices[1:]:
+        if price < start:
+            start = price
+        else:
+            profit = price - start
+            if profit > max_profit:
+                max_profit = profit
+    return max_profit
+def maxProfit2(prices):
+    """
+    :type prices: List[int]
+    :rtype: int
+    """
+    max_profit = 0
+
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            max_profit += prices[i] - prices[i - 1]
+    return max_profit
+
+def canJump(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    # O(n^2) O(n)
+    if len(nums) == 1:
+        return True
+    target = len(nums) - 1
+    dp_list = [False] * len(nums)
+    dp_list[0] = True
+
+    for i in range(len(nums) - 1):
+        if not dp_list[i]:
+            continue
+        for each_jump in range(nums[i]):
+            reach = each_jump + i + 1
+            if reach < target:
+                dp_list[reach] = True
+            elif reach == target:
+                return True
+            elif reach > target:
+                break
+    return dp_list[-1]
+
+    # Alternative (faster and simpler) O(n) O(1)
+    n = len(nums)
+
+    goal = n - 1 
+
+    for i in range(n - 2, -1, -1):
+        if nums[i] + i >= goal:
+            goal = i
+            
+    return goal == 0
+
+def jump( nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    target = len(nums) - 1
+    dp_list = [0] * len(nums)
+
+    for i in range(len(nums) - 1):
+        if dp_list[i] == 0 and i != 0:
+            continue
+        for each_jump in range(nums[i]):
+            reach = each_jump + i + 1
+            if reach <= target:
+                if dp_list[reach] == 0:
+                    dp_list[reach] = dp_list[i] + 1
+                else:
+                    dp_list[reach] = min(dp_list[i] + 1, dp_list[reach])
+            elif reach > target:
+                break
+    return dp_list[-1]
